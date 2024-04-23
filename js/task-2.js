843f36e724376fdb14cdf9e938194ef125d18208
Yanpodolian <153991280+Yanpodolian@users.noreply.github.com>
2024-04-23 19:21:31 +0200

    1

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+function makeArray (firstArray,secondArray,maxLength){
+
+const make = firstArray.concat(secondArray);
+
+//const make = [].concat(firstArray,secondArray);
+
+const make1 = make.slice(0,maxLength)
+    return make1;
+}
+
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
